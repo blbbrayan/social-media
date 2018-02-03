c62326cbbb85f1a489fce82ecbc7d9cb0f05354c
Brian Byrdsong <brian.byrdsong@gmail.com>
2018-02-03 11:51:25 -0500

    settimeout wrapper for loadpage

--- core.js
+++ core.js
@@ -1,47 +1,42 @@
 ((global, fetch) => {
-
-    class core{
-
-        constructor(){
-            global.onload = () => {
-                this.loadPage('home');
-            }            
-        }
-
-        async loadFile(path){
-            return fetch(path).then(res=>res.text()).catch(er => {
-                console.error('GM_ERROR 1\n' + er);
-                return false;
-            });
-        }
-
-        async loadPage(name){
-            const
-                path = `./pages/${name}/${name}`,
-                template = await this.loadFile(`${path}.html`),
-                page = document.createElement('section'),
-                script = document.createElement('script'),
-                link = document.createElement('link');
-
-            console.log(page);
-            page.id = 'page';
-            page.innerHTML = template;
-            document.body.appendChild(page);
-
-            link.id = 'link';
-            link.rel = 'stylesheet';
-            link.href = `${path}.css`;
-            document.body.appendChild(link);
-
-            //To ensure the page is loaded before loading the script
-            // setTimeout(()=>{},0); // use the window.onload event instead, in the constructor
-
-            script.id = 'script';
-            script.src = `${path}.js`;
-            document.body.appendChild(script);
-        }
+  class core {
+    constructor() {
+      this.loadPage("home");
     }
 
-    new core();
+    async loadFile(path) {
+      return fetch(path)
+        .then(res => res.text())
+        .catch(er => {
+          console.error("GM_ERROR 1\n" + er);
+          return false;
+        });
+    }
 
+    async loadPage(name) {
+      setTimeout(async () => {
+        const path = `./pages/${name}/${name}`,
+          template = await this.loadFile(`${path}.html`),
+          page = document.createElement("section"),
+          script = document.createElement("script"),
+          link = document.createElement("link");
+
+        console.log(page);
+        page.id = "page";
+        page.innerHTML = template;
+        document.body.appendChild(page);
+
+        link.id = "link";
+        link.rel = "stylesheet";
+        link.href = `${path}.css`;
+        document.body.appendChild(link);
+
+        script.id = "script";
+        script.src = `${path}.js`;
+        document.body.appendChild(script);
+      }, 0);
+    }
+  }
+
+  new core();
 })(window, window.fetch);
